39d56ceadc08cf0baa3704fce2c452596a89a0e4
GEORGIANAANTON <GEORGIANAANTON@9.145.184.181>
2021-01-26 09:20:11 +0200

    test

--- src/main/java/com/georgiana/certification/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/georgiana/certification/eurekaserver/EurekaServerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+	
+	//test
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
